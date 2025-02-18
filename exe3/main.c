bd232469723518c44310609d1201924078cd9d65
viniciusbp7 <vinicius.b.pomilio@gmail.com>
2025-02-18 15:24:15 -0300

    exs 3

--- exe3/main.c
+++ exe3/main.c
@@ -4,13 +4,16 @@
 
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
+volatile int flag=0;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
-    if (gpio == BTN_PIN_R)
-      printf("fall red\n");
-    else if (gpio == BTN_PIN_G)
-      printf("fall green\n");
+    if (gpio == BTN_PIN_R){
+      flag=1;
+    }
+    else if (gpio == BTN_PIN_G){
+      flag=2;
+    }
   }
 }
 
@@ -25,13 +28,18 @@ int main() {
   gpio_set_dir(BTN_PIN_G, GPIO_IN);
   gpio_pull_up(BTN_PIN_G);
 
-  // callback led r (first)
-  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                     &btn_callback);
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,&btn_callback);
 
-  // callback led g (nao usar _with_callback)
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
   while (true) {
+    if(flag==1){
+      flag=0;
+      printf("fall red \n");
+    }
+    else if(flag==2){
+      flag=0;
+      printf("fall green \n");
+    }
   }
 }
